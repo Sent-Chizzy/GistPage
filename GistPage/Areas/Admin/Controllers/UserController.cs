10bf07f6fa8a06424a1452b860946bea23cb8127
Godsent Ukomadu <Godsentukomadu@outlook.com>
2023-06-04 11:57:32 +0100

    Admin, list of user added. pt 11

--- GistPage/Areas/Admin/Controllers/UserController.cs
+++ GistPage/Areas/Admin/Controllers/UserController.cs
@@ -1,13 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using GistPage.Models;
+using GistPage.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GistPage.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly INotyfService _notification;
+        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, INotyfService notyfService)
         {
-            return View();
+            _userManager = userManager;
+            _signInManager= signInManager;
+            _notification= notyfService;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var vm = users.Select(x => new UserVM()
+            {
+                Id= x.Id,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                UserName = x.UserName,
+            }).ToList();
+            
+            return View(vm);
+        }
+
+        [HttpGet("Login")]
+        public IActionResult Login()
+        {
+            if (!HttpContext.User.Identity!.IsAuthenticated)
+            {
+                return View(new LoginVM());
+            }
+            return RedirectToAction("Index", "User", new {area ="Admin"});
+            
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            var existingUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == vm.Username);
+            if(existingUser == null)
+            {
+                _notification.Error("Username Does not exist");
+                return View(vm);
+            }
+            var verifyPassword = await _userManager.CheckPasswordAsync(existingUser, vm.Password);
+            if(!verifyPassword)
+            {
+                _notification.Error("Password Mismatch");
+                return View(vm);
+            }
+            await _signInManager.PasswordSignInAsync(vm.Username,vm.Password, vm.RememberMe, true);
+            _notification.Success("Login Successful");
+            return RedirectToAction("Index", "User", new {area = "Admin"});
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
+            _notification.Success("Successfully Logged Out");
+            return RedirectToAction("Index", "Home", new {area= ""});
         }
     }
 }
--- GistPage/Program.cs
+++ GistPage/Program.cs
@@ -1,3 +1,5 @@
+using AspNetCoreHero.ToastNotification;
+using AspNetCoreHero.ToastNotification.Extensions;
 using GistPage.Data;
 using GistPage.Models;
 using GistPage.Utilities;
@@ -18,6 +20,13 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 
 builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 
+builder.Services.AddNotyf(config => { config.DurationInSeconds = 10; config.IsDismissable = true; config.Position = NotyfPosition.BottomRight; });
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Login";
+});
+
 var app = builder.Build();
 DataSeeding();
 
@@ -27,10 +36,15 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseNotyf();
+
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
